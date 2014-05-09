9b71125bbbcf23de3584df20b55e6b1840393615
Massimo Siani <massimo.siani@skysql.com>
2014-05-09 09:24:15 -0400

    Fix my.cnf location

--- skysql/mysql-config.sh
+++ skysql/mysql-config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export my_cnf_file="/etc/my.cnf"
+if [[ "$linux_name" == "CentOS" ]] ; then
+	my_cnf_file="/etc/my.cnf"
+elif [[ "$linux_name" == "Debian" || "$linux_name" == "Ubuntu" ]] ; then
+	my_cnf_file="/etc/mysql/my.cnf"
+fi
+export my_cnf_file
+
 export backups_path="/var/backups"
